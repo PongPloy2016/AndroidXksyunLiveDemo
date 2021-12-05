9b527011273cc4fbb8847bb4c260ca1d821a1cc9
ponglang petrung <ponglang_pet@freewillsolutions.com>
2021-12-06 01:58:25 +0700

    Added my project

---
+++ app/src/main/java/com/ksyun/live/demo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.ksyun.live.demo;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ksyun.live.demo.player.activity.PlayerActivity;
+import com.ksyun.media.streamer.demo.DemoActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_stream;
+    private Button btn_player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_player = (Button)findViewById(R.id.btn_player);
+        btn_stream = (Button)findViewById(R.id.btn_stream);
+
+        btn_stream.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,DemoActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_player.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,PlayerActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
